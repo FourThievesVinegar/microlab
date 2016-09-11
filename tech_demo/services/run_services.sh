3deb654a736ee0822f44c49c47fc4e45019b232f
Patrick Ma <fivetwentysix@gmail.com>
2016-09-10 18:36:09 -0700

    dockerfile runs all the stub services

--- tech_demo/services/run_services.sh
+++ tech_demo/services/run_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uwsgi -s /tmp/uwsgi-syringes.sock --plugin python --manage-script-name --mount /syringes=syringe_service:app --chmod-socket=666 --enable-threads &
-uwsgi -s /tmp/uwsgi-stirrers.sock --plugin python --manage-script-name --mount /stirrers=stirrer_service:app --chmod-socket=666 &
-uwsgi -s /tmp/uwsgi-heaters.sock --plugin python --manage-script-name --mount /heaters=heater_service:app --chmod-socket=666 --enable-threads --master &
-
+python heater_service.py &
+python syringe_service.py &
+python stirrer_service.py &
+nginx
