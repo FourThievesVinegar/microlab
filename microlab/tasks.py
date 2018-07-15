315056916ab0717de3dde0d1c4820af0c522f9ca
Brandon Roberts <brandon@bxroberts.org>
2018-07-15 09:55:49 -0700

    Move heater process to async task

--- microlab/tasks.py
+++ microlab/tasks.py
@@ -1,5 +1,12 @@
+from time import sleep
+
 from celery import Celery
+
 from microlab.settings import AMPQ_PASS
+from microlab.services.heater.control import (
+    read_temp, set_heater_operation
+)
+
 
 
 app = Celery(
@@ -8,7 +15,27 @@ app = Celery(
 )
 
 
+# TODO: we will need a shared database to do this properly
 @app.task
-def add(x, y):
-        return x + y
+def heater_update():
+    while True:
+        # TODO: update each heater per HEATERS file, right now
+        # we can only use one (the first)
+        id = tuple(HEATERS.keys())[0]
+        heater = HEATERS[id]
+        print("Heater update ID %s" % id)
+        heater['currenttemp'] = read_temp()
+
+        currentstate = heater["currentstate"]
+        currenttemp = heater["currenttemp"]
+        settemp = heater["settemp"]
+        currentop = heater["currentoperation"]
+
+        if currentstate == "enabled":
+            if currenttemp < settemp and currentop != "heating":
+                set_heater_operation("on")
+            elif currenttemp >= settemp and currentop != "idle":
+                set_heater_operation("off")
+
+        sleep(0.5)
 
